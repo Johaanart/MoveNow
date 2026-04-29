94406e853b263a4570765d980046f389ef24a485
Johan Arturo Gonzalez Lemus <166328905+Johaanart@users.noreply.github.com>
2026-04-28 23:37:30 -0500

    refactor:Analisis de xml.

--- app/src/main/java/ui/auth/VerifyEmailActivity.java
+++ app/src/main/java/ui/auth/VerifyEmailActivity.java
@@ -1,4 +1,4 @@
-package com.example.movenow;
+package ui.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.movenow.MainActivity;
+import com.example.movenow.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
