d8d25c485498544ba42e5754e97126b9296d14c6
Johan Arturo Gonzalez Lemus <166328905+Johaanart@users.noreply.github.com>
2026-04-29 00:05:44 -0500

    refactor:Mover Metas a la subcarpeta Model

--- app/src/main/java/model/Metas.java
+++ app/src/main/java/model/Metas.java
@@ -1,10 +1,12 @@
-package com.example.movenow;
+package model;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+
+import com.example.movenow.R;
 
 
 public class Metas extends Fragment {
